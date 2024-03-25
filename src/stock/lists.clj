15ce8b4918211c39110ef44322b2393909340f4c
hotequil <hotequil@protonmail.com>
2024-03-25 20:54:53 -0300

    feat: use map, range and filter

--- src/stock/lists.clj
+++ src/stock/lists.clj
@@ -26,3 +26,21 @@
   (* value 5))
 
 (println "Prices ->" (update prices 0 multiply-by-five))
+
+(defn multiply-by-two
+  "Multiply a value by two"
+  [value]
+  (* value 2))
+
+(println "Using map ->" (map multiply-by-two prices))
+
+(println "Using range ->" (range 10))
+
+(defn is-bigger-than-fifty?
+  "Return if value is bigger than fifty"
+  [value]
+  (> value 50))
+
+(println "Using filter ->" (filter is-bigger-than-fifty? prices))
+
+(println "Using map after filter ->" (map multiply-by-two (filter is-bigger-than-fifty? prices)))
