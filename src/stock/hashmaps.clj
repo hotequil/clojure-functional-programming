27cbe2cf6c4c995a4f315a4fe5e7ca0ec2343dfa
hotequil <hotequil@protonmail.com>
2024-03-25 22:43:48 -0300

    feat: use threading

--- src/stock/hashmaps.clj
+++ src/stock/hashmaps.clj
@@ -13,3 +13,31 @@
 (println "Products with update ->" (update products :pencil (fn [value] (* value 5))))
 (println "Products with update ->" (update products :pencil #(* % 10)))
 (println "Products with dissoc ->" (dissoc products :pencil))
+
+(def orders {
+  :pencil { :price 2 :quantity 5 }
+  :rubber { :price 1 :quantity 18 }})
+
+(println "Orders ->" orders)
+
+(def orders (assoc orders :notebook { :price 16 :quantity 4 }))
+
+(println "Orders ->" orders)
+(println "Pencil order ->" (orders :pencil))
+(println "Pencil order ->" (get orders :pencil))
+(println "Pencil order ->" (:pencil orders))
+(println "Pen order ->" (orders :pen))
+(println "Pen order ->" (orders :pen { :price nil :quantity 0 }))
+(println "Pen order ->" (get orders :pen))
+(println "Pen order ->" (get orders :pen { :price nil :quantity 0 }))
+(println "Pen order ->" (:pen orders))
+(println "Pen order ->" (:pen orders { :price nil :quantity 0 }))
+
+(println "Quantity pencil ->" (:quantity (:pencil orders)))
+(println "Quantity pen ->" (:quantity (:pen orders) 0))
+
+(println "Pencil update quantity ->" (update-in orders [:pencil :quantity] inc))
+
+(println "Threading in pencil quantity ->" (-> orders :pencil :quantity))
+
+(-> orders :pencil :quantity println)
